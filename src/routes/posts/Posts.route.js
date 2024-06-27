2297c4892de94213b916063d2c582f9f85ebe1c9
wayne <qleonnex@gmail.com>
2024-06-27 16:15:19 +0500

    ⭕ changed the name of the folders in the imports (error during deployment) [part 2]

--- src/routes/posts/Posts.route.js
+++ src/routes/posts/Posts.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import PostsController from './posts.controller.js';
+import PostsController from './Posts.controller.js';
 import { postCreateValidate, postUpdateValidate } from '../../middlewares/validates/posts.validate.js';
 
 const route = Router();
